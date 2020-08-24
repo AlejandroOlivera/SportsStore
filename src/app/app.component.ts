f6b836a693116e66df59abb4cb4d78b6afeb6e70
AlejandroG <aienma325@gmail.com>
2020-08-24 18:29:59 -0500

    viewCategory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app',
+  template: '<store></store>',
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'SportsStore';
-}
+export class AppComponent {}
