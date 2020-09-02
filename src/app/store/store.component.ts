9c3674ffbf0386a90dc2d08ff26d6b4eb75ad674
AlejandroG <aienma325@gmail.com>
2020-09-02 14:52:23 -0500

    fixCategories

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,9 +1,8 @@
-import {Component} from '@angular/core';
-import {Product} from '../model/product.model';
-import {ProductRepository} from '../model/product.repository';
-import {Cart} from '../model/cart.model';
-import {Router} from '@angular/router';
-
+import { Component } from '@angular/core';
+import { Product } from '../model/product.model';
+import { ProductRepository } from '../model/product.repository';
+import { Cart } from '../model/cart.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'store',
@@ -15,8 +14,11 @@ export class StoreComponent {
   public productsPerPage = 4;
   public selectedPage = 1;
 
-  constructor(private repository: ProductRepository, private cart: Cart, private router: Router) {
-  }
+  constructor(
+    private repository: ProductRepository,
+    private cart: Cart,
+    private router: Router
+  ) {}
 
   get products(): Product[] {
     let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
@@ -39,13 +41,13 @@ export class StoreComponent {
 
   changePageSize(newSize: number) {
     this.productsPerPage = Number(newSize);
-    this.changePage(1);
+    this.changePage(1)
   }
 
   get pageCount(): number {
     return Math.ceil(
       this.repository.getProducts(this.selectedCategory).length /
-      this.productsPerPage
+        this.productsPerPage
     );
   }
 
